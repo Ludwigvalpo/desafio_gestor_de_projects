cc6ed105b6c2e9b1e67d37a2b00b6320c966be0b
Ludwig Valdenegro <ludwigval@gmail.com>
2022-07-07 19:53:41 -0400

    reparación de falla de sintaxis

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
-
+    validates :name, :description, presence: true
     enum state: [:proposal, :in_progress, :finished]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'project/index'
+  get 'project/create'
 
   root 'project#index'
 end
